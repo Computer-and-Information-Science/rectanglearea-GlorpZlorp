2d2cd1d12ac5a351e4f7ea988a983db27b0758b9
GlorpZlorp <joshuaholinka10@gmail.com>
2025-09-08 01:47:32 +0000

    calculated rectangle area.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  const double length = 7.2;
-  const double width = 1.3;
+int main() 
+{
+  double length;
+  double width;
+  double area;
+  double perimeter;
+  
+  cout << "Rectangle Properties." << endl;
 
+  length = 7.2;
+  width = 1.3;
+  perimeter = 2 * (7.2 + 1.3);
+  area = 7.2 * 1.3;
 
+  cout << "Length = " << length << endl; 
+  cout << "Width = " << width << endl;
+  cout << "Perimeter = " << perimeter << endl;
+  cout << "Area = " << area << endl;
+
+  return 0;
 }
